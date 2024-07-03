9da02c7bbed8746d197ae86b341f7e32d2829a7c
Pearl Dsilva <pearl1594@gmail.com>
2024-07-03 15:30:46 -0400

    set source cudr list

--- cloudstack_loadbalancer.go
+++ cloudstack_loadbalancer.go
@@ -812,9 +812,6 @@ func (lb *loadBalancer) updateNetworkACL(publicPort int, protocol LoadBalancerPr
 		return false, fmt.Errorf("error fetching Network with ID: %v, due to: %s", networkId, err)
 	}
 
-	// Get All ACL rules for the given ACL ID
-	lb.NetworkACL.ListNetworkACLLists()
-
 	// create ACL rule
 	acl := lb.NetworkACL.NewCreateNetworkACLParams(protocol.CSProtocol())
 	acl.SetAclid(network.Aclid)
